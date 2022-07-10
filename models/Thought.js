d765b6dc61af4b7dfa9ac2ca33ce84c5609d8861
stevey d <stphndcky22@gmail.com>
2022-07-10 12:39:55 -0700

    Created User, Thought, and Reaction models. Created formatDate.js helper

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+;
+const reactionSchema = require('./Reaction');
+
+const formatDate = require('../utils/formatDate');
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "Can't post empty Thought!",
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => formatDate(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  },
+);
+
+ThoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
